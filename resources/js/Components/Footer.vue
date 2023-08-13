e2915a17b0b465e026a777c417f75ca7d352f81b
Lenfumé Julien <julabina@hotmail.fr>
2023-08-13 12:41:17 +0200

    responsive - footer

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-    <footer class="flex items-center justify-center mx-auto mt-12 h-20 bg-primary text-white">
-        <section class="flex justify-between max-w-7xl w-full">
-            <p class="text-sm font-light">Copyright © suivrepoids.fr - 2022</p>
-            <nav class="flex">
-                <!-- <Link :href="route('legals.about')" class="ml-5 hover:underline"><p>A propos</p></Link> -->
-                <Link :href="route('legals.cgu')" class="ml-5 hover:underline"><p>CGU</p></Link>
-                <Link :href="route('legals.contact')" class="ml-5 hover:underline"><p>Contact</p></Link>
+    <footer class="flex items-center justify-center mx-auto mt-12 md:h-20 py-8 md:py-0 bg-primary text-white">
+        <section class="flex flex-col-reverse md:flex-row items-center justify-between px-4 lg:px-0 lg:max-w-7xl w-full">
+            <p class="text-sm font-light mt-8 md:mt-0 text-center md:text-left">Copyright © suivrepoids.fr - 2022</p>
+            <nav class="flex flex-col md:flex-row">
+                <!-- <Link :href="route('legals.about')" class="md:ml-5 text-center md:text-left hover:underline"><p>A propos</p></Link> -->
+                <Link :href="route('legals.cgu')" class="md:ml-5 text-center md:text-left hover:underline font-semibold text-lg mb-2 md:font-normal md:text-base md:mb-0"><p>CGU</p></Link>
+                <Link :href="route('legals.contact')" class="md:ml-5 text-center md:text-left hover:underline font-semibold text-lg mb-2 md:font-normal md:text-base md:mb-0"><p>Contact</p></Link>
             </nav>
         </section>
     </footer>
